af5ee7cb0622c1ba86d14d6582176c291a5b6974
pryority <matthewapryor@gmail.com>
2022-07-07 13:24:57 -0400

    deploy website to gh pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import * as React from 'react';
 import Nav from './navigation/Navbar.js';
 import Footer from './navigation/Footer.js';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.js';
 import About from './pages/About.js';
 import Portfolio from './pages/Portfolio';
